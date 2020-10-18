248b20d4a116236b4d17c1820f8e960c29e5cc0d
tanahara-takuya <tanahara.2361@gmail.com>
2020-10-18 22:35:57 +0900

    vue2-google-maps導入

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,7 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
+    {src:"~plugins/vue2-google-maps.js"}
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
@@ -59,5 +60,6 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    transpile: [/^vue2-google-maps($|\/)/]
   }
 }
